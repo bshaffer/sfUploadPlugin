b0393fb24df2bd0ee2ba19a75f29aab99ee6f25b
Brent Shaffer <bshafs@gmail.com>
2010-10-14 15:37:06 -0500

    adds display none to widget

--- lib/widget/sfWidgetFormInputFileMultiple.class.php
+++ lib/widget/sfWidgetFormInputFileMultiple.class.php
@@ -8,9 +8,9 @@ class sfWidgetFormInputFileMultiple extends sfWidgetFormInputFile
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
     $widget     = new sfWidgetFormInputFile($this->options, $this->attributes);
-    $prototype  = $widget->render($name.'[]', null, array_merge($attributes, array('disabled' => true)));
+    $prototype  = $widget->render($name.'[]', null, array_merge(array('disabled' => true), $attributes));
     
-    $html       = $this->renderContentTag('div', $prototype, array('class' => 'prototype'));
+    $html       = $this->renderContentTag('div', $prototype, array('class' => 'prototype', 'style' => 'display:none'));
     
     if (!$value) 
     {
